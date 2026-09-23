360cd27961288039f13f897a417b07a1c45af8af
Daryana <dpazdniakova@gmail.com>
2026-09-23 12:49:55 +0100

    Lab 2b - Done

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -16,7 +16,8 @@ function App() {
     const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: ""
   });
 
     const formChangeHandler = (event) => {
@@ -32,6 +33,9 @@ function App() {
       case "deadline":
           form.deadline = event.target.value;
           break;
+      case "priority":
+        form.priority = event.target.value;
+           break;
       default:
           form = formState;
     }
